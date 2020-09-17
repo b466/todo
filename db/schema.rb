ea54d035e6c995ec340ca42b807b2fdb874da9e5
kenta <kenta@kentanoMacBook-Pro.local>
2020-09-17 22:28:40 +0900

    [add]user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_17_130203) do
+ActiveRecord::Schema.define(version: 2020_09_17_132145) do
 
   create_table "lists", force: :cascade do |t|
     t.string "title", null: false
@@ -18,6 +18,12 @@ ActiveRecord::Schema.define(version: 2020_09_17_130203) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_lists_on_user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "name", default: "", null: false
   end
 
 end
