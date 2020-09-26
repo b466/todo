9a23834f24baeddf86e371448fd4014562e43e3d
kenta <kenta@kentanoMacBook-Pro.local>
2020-09-26 23:45:18 +0900

    [add]カラム

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_24_143907) do
+ActiveRecord::Schema.define(version: 2020_09_26_143828) do
 
   create_table "lists", force: :cascade do |t|
     t.string "title", null: false
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2020_09_24_143907) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name", default: "", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
