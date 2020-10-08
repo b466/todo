506f83fda898418fccc6651664e3f5975b2c608a
kenta <kenta@kentanoMacBook-Pro.local>
2020-10-08 23:57:25 +0900

    [add]destroy

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 root'top#index'
 
-resources :list, only: %i(new create edit update)
+resources :list, only: %i(new create edit update destroy)
 end
