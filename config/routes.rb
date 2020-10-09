f1aedb46597df69faa6639e535630cee4e5f735b
kenta <kenta@kentanoMacBook-Pro.local>
2020-10-09 17:20:08 +0900

    [add]ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 root'top#index'
 
-resources :list, only: %i(new create edit update destroy)
+resources :list, only: %i(new create edit update destroy)do
+resources :card, only: %i(new create)
+end
+
+
 end
